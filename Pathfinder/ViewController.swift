fb2a753e0db815119506a8d3bb16605911e53fa1
윤준수 <oso0103@gmail.com>
2021-05-22 03:25:03 +0900

    Import NaverMap, Current Location

--- Pathfinder/ViewController.swift
+++ Pathfinder/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import NMapsMap
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var naverMapView: NMFNaverMapView!
+    var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+//        print(locationManager.location)
+        naverMapView.showLocationButton = true
         // Do any additional setup after loading the view.
+//        let mapView = NMFMapView(frame: view.frame)
+//        view.addSubview(mapView)
     }
 
 
+}
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard locations.count != 0 else { return }
+        let location = locations[0]
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: location.coordinate.latitude, lng: location.coordinate.longitude))
+        naverMapView.mapView.moveCamera(cameraUpdate)
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Pathfinder' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Pathfinder
+
+pod 'NMapsMap'
+pod 'Alamofire', '~> 5.2'
+pod 'SDWebImage', '~> 5.0'
+
+
+  target 'PathfinderTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PathfinderUITests' do
+    # Pods for testing
+  end
+
+end
